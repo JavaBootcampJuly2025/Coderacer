517459f6b939729e5f0f63362f3b77d715dac187
Jokimax <jonaxthedreadlord@gmail.com>
2025-07-15 13:22:54 +0300

    add password to create account

--- src/main/java/com/coderacer/dto/AccountCreateDTO.java
+++ src/main/java/com/coderacer/dto/AccountCreateDTO.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 public record AccountCreateDTO (
         String username,
-        String email
+        String email,
+        String password
         /// NOT SURE IF THIS IS IT HERE
 ){}
