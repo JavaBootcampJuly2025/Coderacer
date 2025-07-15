36abcb7334f4e3d030e9acb5da41d915259554bc
Jokimax <jonaxthedreadlord@gmail.com>
2025-07-15 12:06:04 +0300

    move to models

---
+++ src/main/java/com/coderacer/models/AccountMetrics.java
@@ -0,0 +1,30 @@
+package com.coderacer.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Subselect;
+import org.springframework.data.annotation.Immutable;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Immutable
+//@Subselect() finish later
+public class AccountMetrics {
+    @Id
+    private UUID id;
+
+//    @OneToOne
+//    @JoinColumn(name = "account_id")
+//    private Account account;
+
+    private double avgCpm;
+    private double avgAccuracy;
+    private double avgCpmWeekly;
+    private double avgAccuracyWeekly;
+}
