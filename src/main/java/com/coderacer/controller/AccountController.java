9ef664f0287f4e98aebffa670dc540941c0a676e
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-07-15 14:51:17 +0300

    Account controller added

--- src/main/java/com/coderacer/controller/AccountController.java
+++ src/main/java/com/coderacer/controller/AccountController.java
@@ -1,18 +1,14 @@
 package com.coderacer.controller;
 
-
-import com.coderacer.dto.AccountCreateDTO;
-import com.coderacer.dto.AccountDTO;
+import com.coderacer.dto.*;
 import com.coderacer.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.security.auth.login.AccountNotFoundException;
 import java.net.URI;
 import java.util.List;
 import java.util.UUID;
@@ -20,38 +16,29 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/accounts")
 @RequiredArgsConstructor
-
 public class AccountController {
 
     private final AccountService accountService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<AccountDTO> getAccount(@PathVariable UUID id) throws AccountNotFoundException {
+    public ResponseEntity<AccountDTO> getAccount(@PathVariable UUID id) {
         return ResponseEntity.ok(accountService.getAccount(id));
     }
 
-    @GetMapping("/account-username/{username}")
-    public ResponseEntity<AccountDTO> getAccountByUsername(@PathVariable String username){
+    @GetMapping("/username/{username}")
+    public ResponseEntity<AccountDTO> getAccountByUsername(
+            @PathVariable String username) {
         return ResponseEntity.ok(accountService.getAccountByUsername(username));
     }
 
-    @GetMapping("/account-email/{email}")
-    public ResponseEntity<AccountDTO> getAccountByEmail(@PathVariable String email){
-        return ResponseEntity.ok(accountService.getAccountByEmail(email));
-    }
-
-    @GetMapping("/account-rank/{rank}")
-    public ResponseEntity<AccountDTO> getAccountByRank(@PathVariable String rank){
-        return ResponseEntity.ok(accountService.getAccountByRank(rank));
-    }
-
     @GetMapping
-    public ResponseEntity<List<AccountDTO>> getAllAccounts(){
+    public ResponseEntity<List<AccountDTO>> getAllAccounts() {
         return ResponseEntity.ok(accountService.getAllAccounts());
     }
 
     @PostMapping
-    public ResponseEntity<AccountDTO> createAccount(@RequestBody @Valid AccountCreateDTO accountCreateDTO){
+    public ResponseEntity<AccountDTO> createAccount(
+            @RequestBody @Valid AccountCreateDTO accountCreateDTO) {
         AccountDTO created = accountService.createAccount(accountCreateDTO);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -61,22 +48,30 @@ public class AccountController {
         return ResponseEntity.created(location).body(created);
     }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<AccountDTO> updateAccount(
-//            @PathVariable UUID id,
-//            @RequestBody @Valid AccountUpdateDTO accountUpdateDTO){
-//        return ResponseEntity.ok(accountService.updateAcount(id,accountUpdateDTO));
-//    }
-    /// NOT SURE IF WE NEED UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<AccountDTO> updateAccount(
+            @PathVariable UUID id,
+            @RequestBody @Valid AccountUpdateDTO accountUpdateDTO) {
+        return ResponseEntity.ok(accountService.updateAccount(id, accountUpdateDTO));
+    }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteAccount(@PathVariable UUID id){
+    public void deleteAccount(@PathVariable UUID id) {
         accountService.deleteAccount(id);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<AccountDTO> restoreAccount(@PathVariable UUID id){
-        return ResponseEntity.ok(accountService.restoreAccount(id));
+    @PutMapping("/{id}/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(
+            @PathVariable UUID id,
+            @RequestBody @Valid PasswordChangeDTO passwordChangeDTO) {
+        accountService.changePassword(id, passwordChangeDTO);
+    }
+
+    @PutMapping("/{id}/verify")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void verifyAccount(@PathVariable UUID id) {
+        accountService.verifyAccount(id);
     }
 }
