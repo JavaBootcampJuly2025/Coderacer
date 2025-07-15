6b5cb5c7bf77ec10791894d208b7c53c963088c3
Kristautas Lebedis <kristautaslebedis@gmail.com>
2025-07-15 10:27:24 +0300

    Added Account model and AccountRepository

---
+++ src/main/java/com/coderacer/model/Account.java
@@ -0,0 +1,30 @@
+package com.coderacer.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Setter
+@Getter
+@Table(indexes = {
+        @Index(name = "idx_username", columnList = "username")
+        })
+public class Account {
+    @Id
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String hashedPassword;
+    private int rating;
+    private boolean verified;
+}
+
+
