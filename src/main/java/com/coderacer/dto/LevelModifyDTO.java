5af00d0c57c245dec8514b6b8627da7b0ddcecc2
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-07-15 12:58:02 +0300

    Brought the regex annotation from entity to be in the DTO too

--- src/main/java/com/coderacer/dto/LevelModifyDTO.java
+++ src/main/java/com/coderacer/dto/LevelModifyDTO.java
@@ -2,10 +2,7 @@ package com.coderacer.dto;
 
 import com.coderacer.enums.Difficulty;
 import com.coderacer.enums.ProgrammingLanguage;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.List;
 
@@ -13,5 +10,5 @@ public record LevelModifyDTO(
         @NotBlank @Size(min = 100, max = 2000) String codeSnippet,
         @NotNull ProgrammingLanguage language,
         @NotNull Difficulty difficulty,
-        List<@NotBlank @Size(max = 20) String> tags
+        List<@NotBlank @Size(max = 20) @Pattern(regexp = "^[a-zA-Z]+$") String> tags
 ) {}
