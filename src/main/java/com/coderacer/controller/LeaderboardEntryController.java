e7379eed86c8665cafe30b7c5cb72f710d3555b0
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-07-16 14:52:56 +0300

    LeaderboardEntryController added

--- src/main/java/com/coderacer/controller/LeaderboardEntryController.java
+++ src/main/java/com/coderacer/controller/LeaderboardEntryController.java
@@ -1,9 +1,13 @@
 package com.coderacer.controller;
 
-import com.coderacer.model.LeaderboardEntry;
+import com.coderacer.dto.LeaderboardEntryDTO;
 import com.coderacer.service.LeaderboardEntryService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +15,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/leaderboard")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class LeaderboardEntryController {
-    private final LeaderboardEntryService service;
+
+    private final LeaderboardEntryService leaderboardService;
+
+    @GetMapping("/top")
+    public ResponseEntity<List<LeaderboardEntryDTO>> getTop10() {
+        return ResponseEntity.ok(leaderboardService.getTop10());
+    }
 
     @GetMapping
-    public List<LeaderboardEntry> getLeaderboard() {
-        return service.getLeaderboardEntries();
+    public ResponseEntity<Page<LeaderboardEntryDTO>> getLeaderboard(Pageable pageable) {
+        return ResponseEntity.ok(leaderboardService.getLeaderboard(pageable));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<LeaderboardEntryDTO> getByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(leaderboardService.getByUsername(username));
     }
 }
