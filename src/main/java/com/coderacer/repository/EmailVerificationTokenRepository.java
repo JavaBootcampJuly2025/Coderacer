2c6019220fe27ae99c968b6b873af25eff33bbc1
Jokimax <77680901+Jokimax@users.noreply.github.com>
2025-07-18 11:50:05 +0300

    implement automatic email verification token deletion (#40)
    * implement automatic email verification token deletion
    
    * implement automatic email verification token deletion

--- src/main/java/com/coderacer/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/coderacer/repository/EmailVerificationTokenRepository.java
@@ -3,6 +3,10 @@ package com.coderacer.repository;
 import com.coderacer.model.EmailVerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface EmailVerificationTokenRepository extends JpaRepository<EmailVerificationToken, Long> {
     EmailVerificationToken findByToken(String token);
+    List<EmailVerificationToken> findByExpiryDateBefore(LocalDateTime time);
 }
