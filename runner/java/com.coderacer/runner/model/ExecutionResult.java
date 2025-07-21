cf841892111660348cf21c5ac74e4201acfd028f
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-07-21 11:15:20 +0300

    Execution result model

---
+++ runner/java/com.coderacer/runner/model/ExecutionResult.java
@@ -0,0 +1,46 @@
+package com.coderacer.runner.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExecutionResult {
+    
+    public enum Result {
+        SUCCESS,
+        OUTPUT_MISMATCH,
+        COMPILATION_ERROR,
+        RUNTIME_ERROR,
+        TIMEOUT
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
+
+    /** Overall status of this run */
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Result result = Result.TIMEOUT;
+
+    /** Counted by lines. I want this to be used in frontend as "19/25 tests cases correct" etc. */
+    @Column(nullable = false)
+    private int correctOutputCount = 0;
+    
+    /**
+     * The actual lines of output produced by the user’s code.
+     * Compare this against CodeSubmission.expectedOutputs.
+     */
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private List<String> outputLines = new ArrayList<>();
+}
