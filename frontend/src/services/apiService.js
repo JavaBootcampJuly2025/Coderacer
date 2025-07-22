6140071f3f95e5adfaa1af63fbbfb5293b5418b9
Jokimax <77680901+Jokimax@users.noreply.github.com>
2025-07-22 13:46:32 +0300

    Implement leaderboard (#65)

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -5,12 +5,6 @@ const api = axios.create({
     withCredentials: true,
 });
 
-// Fetch all accounts
-export const getAllAccounts = async () => {
-    const response = await api.get('/api/accounts');
-    return response.data;
-};
-
 // Fetch account by ID
 export const getAccountById = async (id, authToken) => {
     const response = await api.get(`/api/accounts/${id}`, {
@@ -68,5 +62,11 @@ export const getRandomLevelWithParameters = async (language, difficulty) => {
             difficulty,
         }
     });
+    return response.data;
+};
+
+// Get top-rated accounts
+export const getTop = async () => {
+    const response = await api.get('/api/leaderboard/top');
     return response.data;
 };
