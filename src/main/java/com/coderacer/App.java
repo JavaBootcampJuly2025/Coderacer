e5efb727b25d0d1cf694c245c3783a175231c27e
Kostas-Juskevicius <kostas.jusk@gmail.com>
2025-07-22 10:59:50 +0300

    App should run app not runner app :)

--- src/main/java/com/coderacer/App.java
+++ src/main/java/com/coderacer/App.java
@@ -1,6 +1,5 @@
 package com.coderacer;
 
-import com.coderacer.runner.RunnerApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class App {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(RunnerApp.class);
+        SpringApplication app = new SpringApplication(App.class);
         app.setAdditionalProfiles("app");
         app.run(args);
     }
